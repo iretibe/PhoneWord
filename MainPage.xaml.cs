1baf77a60f7b7d13adc5b4e5450e3568d22004eb
Somad <iretibe@users.noreply.github.com>
2022-07-21 12:50:14 +0000

    Pushed the application

---
+++ MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿namespace PhoneWord;
+
+public partial class MainPage : ContentPage
+{
+	string translatedNumber;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnTranslate(object sender, EventArgs e)
+    {
+        var enteredNumber = PhoneNumberText.Text;
+
+        //translatedNumber = Core.PhoneWordTranslator.ToNumber(enteredNumber);
+        translatedNumber = enteredNumber;
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number",
+            "Would you like to call " + translatedNumber + "?",
+            "Yes",
+            "No"))
+        {
+            try
+            {
+                PhoneDialer.Open(translatedNumber);
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+            }
+            catch (Exception)
+            {
+                // Other error has occurred.
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+
+
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //	count += 10;
+
+    //	if (count == 1)
+    //		CounterBtn.Text = $"Clicked {count} time";
+    //	else
+    //		CounterBtn.Text = $"Clicked {count} times";
+
+    //	SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
+}
+
---
+++ Models/PhoneWordTranslator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneWord.Models
+{
+    public static class PhoneWordTranslator
+    {
+        private static string[] digits =
+        {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+            {
+                return null;
+            }
+
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+
+            foreach (var c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                {
+                    newNumber.Append(c);
+                }
+                else
+                {
+                    var result = TranslateToNumber(c, 2);
+
+                    if (result != null)
+                    {
+                        newNumber.Append(result);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return newNumber.ToString();
+        }
+
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        private static int? TranslateToNumber(char c, int v)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                {
+                    return 2 + i;
+                }                
+            }
+
+            return null;
+        }
+    }
+}
